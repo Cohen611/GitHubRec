474bcf38bfa375b2414a6a555004a171744c86d7
Candace Cohen <cecoh611@gmail.com>
2016-02-21 14:49:44 -0500

    added project

---
+++ githubactivity.js
@@ -0,0 +1,670 @@
+var githubactivities =
+[
+{
+id: "3655010206",
+type: "PushEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51875425,
+name: "Cohen611/map-filter",
+url: "https://api.github.com/repos/Cohen611/map-filter"
+},
+payload: {
+push_id: 981296060,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "219ddab9fb2dc5d6177645292d56fdc431e8a7e6",
+before: "37f91d079c553d0b0d9613c93340e89330b44f73",
+commits: [
+{
+sha: "219ddab9fb2dc5d6177645292d56fdc431e8a7e6",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "revised answer6",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/map-filter/commits/219ddab9fb2dc5d6177645292d56fdc431e8a7e6"
+}
+]
+},
+public: true,
+created_at: "2016-02-17T14:55:32Z"
+},
+{
+id: "3652038932",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51875425,
+name: "Cohen611/map-filter",
+url: "https://api.github.com/repos/Cohen611/map-filter"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-16T22:33:58Z"
+},
+{
+id: "3652037236",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51875425,
+name: "Cohen611/map-filter",
+url: "https://api.github.com/repos/Cohen611/map-filter"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-16T22:33:33Z"
+},
+{
+id: "3650716544",
+type: "PushEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51489435,
+name: "Cohen611/Mixology",
+url: "https://api.github.com/repos/Cohen611/Mixology"
+},
+payload: {
+push_id: 979770478,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "175ff4204e77fa91878e447fe96dfba8f13d2282",
+before: "ed9a10ea1bc0303520118b91c1861a50c8508ba2",
+commits: [
+{
+sha: "175ff4204e77fa91878e447fe96dfba8f13d2282",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "refactored",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/Mixology/commits/175ff4204e77fa91878e447fe96dfba8f13d2282"
+}
+]
+},
+public: true,
+created_at: "2016-02-16T17:37:49Z"
+},
+{
+id: "3647414873",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51796018,
+name: "Cohen611/time-to-read",
+url: "https://api.github.com/repos/Cohen611/time-to-read"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-16T00:32:06Z"
+},
+{
+id: "3647413928",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51796018,
+name: "Cohen611/time-to-read",
+url: "https://api.github.com/repos/Cohen611/time-to-read"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-16T00:31:38Z"
+},
+{
+id: "3642854092",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51704366,
+name: "Cohen611/Octocats",
+url: "https://api.github.com/repos/Cohen611/Octocats"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-14T17:19:31Z"
+},
+{
+id: "3642853689",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51704366,
+name: "Cohen611/Octocats",
+url: "https://api.github.com/repos/Cohen611/Octocats"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-14T17:19:09Z"
+},
+{
+id: "3642811628",
+type: "PushEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 50938791,
+name: "Cohen611/Assignment1",
+url: "https://api.github.com/repos/Cohen611/Assignment1"
+},
+payload: {
+push_id: 976842405,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "766fa2a8d1e8901005a4d6b7942f7b75077b55ac",
+before: "680f03c30cda8fb16ba4b77f0396589160c89889",
+commits: [
+{
+sha: "766fa2a8d1e8901005a4d6b7942f7b75077b55ac",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "revised assignment1",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/Assignment1/commits/766fa2a8d1e8901005a4d6b7942f7b75077b55ac"
+}
+]
+},
+public: true,
+created_at: "2016-02-14T16:40:45Z"
+},
+{
+id: "3631575145",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51489435,
+name: "Cohen611/Mixology",
+url: "https://api.github.com/repos/Cohen611/Mixology"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "Javascript- a text based game that will allow the player to make decisions that determine their outcome of their drink.",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-11T02:29:39Z"
+},
+{
+id: "3631570555",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51489435,
+name: "Cohen611/Mixology",
+url: "https://api.github.com/repos/Cohen611/Mixology"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "Javascript- a text based game that will allow the player to make decisions that determine their outcome of their drink.",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-11T02:27:19Z"
+},
+{
+id: "3624962896",
+type: "PushEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51186521,
+name: "Cohen611/sk8tr",
+url: "https://api.github.com/repos/Cohen611/sk8tr"
+},
+payload: {
+push_id: 970381015,
+size: 1,
+distinct_size: 1,
+ref: "refs/heads/master",
+head: "718877bb3c896c45193397962403c52c19357a3f",
+before: "4d9c73aa23eca6123dd81a9c9525fa5917b64140",
+commits: [
+{
+sha: "718877bb3c896c45193397962403c52c19357a3f",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "added slider image",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/sk8tr/commits/718877bb3c896c45193397962403c52c19357a3f"
+}
+]
+},
+public: true,
+created_at: "2016-02-09T18:45:00Z"
+},
+{
+id: "3619104969",
+type: "PushEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51186521,
+name: "Cohen611/sk8tr",
+url: "https://api.github.com/repos/Cohen611/sk8tr"
+},
+payload: {
+push_id: 968325636,
+size: 3,
+distinct_size: 3,
+ref: "refs/heads/master",
+head: "4d9c73aa23eca6123dd81a9c9525fa5917b64140",
+before: "d3f82da61a4f4f4f1a683668284a7542932a800a",
+commits: [
+{
+sha: "271166da810c92390ab30c2b05cae9d835fd6e13",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "corrected code in skater div",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/sk8tr/commits/271166da810c92390ab30c2b05cae9d835fd6e13"
+},
+{
+sha: "3b2f328a2f6dfe20415af8f40bffb100934e4bfd",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "revisions",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/sk8tr/commits/3b2f328a2f6dfe20415af8f40bffb100934e4bfd"
+},
+{
+sha: "4d9c73aa23eca6123dd81a9c9525fa5917b64140",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "adding social icons",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/sk8tr/commits/4d9c73aa23eca6123dd81a9c9525fa5917b64140"
+}
+]
+},
+public: true,
+created_at: "2016-02-08T14:49:27Z"
+},
+{
+id: "3614509230",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51186521,
+name: "Cohen611/sk8tr",
+url: "https://api.github.com/repos/Cohen611/sk8tr"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-06T02:44:09Z"
+},
+{
+id: "3614506417",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51186521,
+name: "Cohen611/sk8tr",
+url: "https://api.github.com/repos/Cohen611/sk8tr"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-06T02:41:39Z"
+},
+{
+id: "3605587802",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51056903,
+name: "Cohen611/surf",
+url: "https://api.github.com/repos/Cohen611/surf"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-04T06:31:27Z"
+},
+{
+id: "3605581820",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 51056903,
+name: "Cohen611/surf",
+url: "https://api.github.com/repos/Cohen611/surf"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-04T06:28:31Z"
+},
+{
+id: "3598262301",
+type: "PushEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 50938791,
+name: "Cohen611/Assignment1",
+url: "https://api.github.com/repos/Cohen611/Assignment1"
+},
+payload: {
+push_id: 960709983,
+size: 3,
+distinct_size: 3,
+ref: "refs/heads/master",
+head: "680f03c30cda8fb16ba4b77f0396589160c89889",
+before: "c047b5af9637080fb27d72547ed3f9872e7bee19",
+commits: [
+{
+sha: "cd68f64049297ce8deed4601d6e99a0d1f2a3b80",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "Added another file",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/Assignment1/commits/cd68f64049297ce8deed4601d6e99a0d1f2a3b80"
+},
+{
+sha: "9b2e6de34ecab5b8262db7f53aff50264f756ccb",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "Adding another file",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/Assignment1/commits/9b2e6de34ecab5b8262db7f53aff50264f756ccb"
+},
+{
+sha: "680f03c30cda8fb16ba4b77f0396589160c89889",
+author: {
+email: "cecoh611@gmail.com",
+name: "Candace Cohen"
+},
+message: "Adding another file",
+distinct: true,
+url: "https://api.github.com/repos/Cohen611/Assignment1/commits/680f03c30cda8fb16ba4b77f0396589160c89889"
+}
+]
+},
+public: true,
+created_at: "2016-02-02T18:45:26Z"
+},
+{
+id: "3597952642",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 50938791,
+name: "Cohen611/Assignment1",
+url: "https://api.github.com/repos/Cohen611/Assignment1"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-02T17:37:37Z"
+},
+{
+id: "3597949730",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 50938791,
+name: "Cohen611/Assignment1",
+url: "https://api.github.com/repos/Cohen611/Assignment1"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-02T17:36:57Z"
+},
+{
+id: "3597881924",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 50937037,
+name: "Cohen611/demoGitRepository",
+url: "https://api.github.com/repos/Cohen611/demoGitRepository"
+},
+payload: {
+ref: "master",
+ref_type: "branch",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-02T17:22:06Z"
+},
+{
+id: "3597814086",
+type: "CreateEvent",
+actor: {
+id: 17028961,
+login: "Cohen611",
+gravatar_id: "",
+url: "https://api.github.com/users/Cohen611",
+avatar_url: "https://avatars.githubusercontent.com/u/17028961?"
+},
+repo: {
+id: 50937037,
+name: "Cohen611/demoGitRepository",
+url: "https://api.github.com/repos/Cohen611/demoGitRepository"
+},
+payload: {
+ref: null,
+ref_type: "repository",
+master_branch: "master",
+description: "",
+pusher_type: "user"
+},
+public: true,
+created_at: "2016-02-02T17:08:03Z"
+}];
